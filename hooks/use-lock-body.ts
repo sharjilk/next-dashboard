f368587fe940becb5d95e607b18ba2a4c4463498
sharjilk <sharjilk62@gmail.com>
2025-05-19 19:46:54 +0530

    Update use-lock-body.ts

--- hooks/use-lock-body.ts
+++ hooks/use-lock-body.ts
@@ -1,14 +1,14 @@
-import { useLayoutEffect } from 'react';
+// import { useLayoutEffect } from 'react';
 
-// @see https://usehooks.com/useLockBodyScroll.
-const useLockBody = () => {
-  useLayoutEffect((): (() => void) => {
-    const originalStyle: string = window.getComputedStyle(
-      document.body
-    ).overflow;
-    document.body.style.overflow = 'hidden';
-    return () => (document.body.style.overflow = originalStyle);
-  }, []);
-};
+// // @see https://usehooks.com/useLockBodyScroll.
+// const useLockBody = () => {
+//   useLayoutEffect((): (() => void) => {
+//     const originalStyle: string = window.getComputedStyle(
+//       document.body
+//     ).overflow;
+//     document.body.style.overflow = 'hidden';
+//     return () => (document.body.style.overflow = originalStyle);
+//   }, []);
+// };
 
-export default useLockBody;
+// export default useLockBody;
